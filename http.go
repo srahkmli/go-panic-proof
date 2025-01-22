f8e2b748ed76d5a153d1b681b90a90f2dc98a01b
younesious <younesious80@gmail.com>
2025-01-23 02:51:11 +0330

    add zap and structaul loggin for httprecover func

--- http.go
+++ http.go
@@ -4,15 +4,23 @@ import (
 	"log"
 	"net/http"
 	"runtime/debug"
+	"time"
+
+	"go.uber.org/zap"
 )
+
+var logger, _ = zap.NewProduction()
 
 // HTTPRecover is an HTTP middleware that recovers from panics and logs the error.
 func HTTPRecover(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				// Log the panic error with stack trace.
-				log.Printf("Recovered from panic: %v\nStack Trace: %s", err, string(debug.Stack()))
+				// Structual log with zap
+				logger.Error("Recovered from panic",
+					zap.Any("error", err),
+					zap.String("stack_trace", string(debug.Stack())),
+					zap.Time("timestamp", time.Now()))
 
 				// Respond with 500 Internal Server Error.
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
